eb0c05695109292f988495a2f669cfc9a12c52e2
Felipe Campos <felipecamposdev@gmail.com>
2019-10-16 13:34:05 -0300

    change value prop Select

--- src/lib/components/Select/Select.jsx
+++ src/lib/components/Select/Select.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyledSelect, Wrapper, StyledIcon } from "./style";
 
-export const Select = ({ options, value, className, style, isError, ...props }) => {
+export const Select = ({ options, defaultValue, className, style, isError, ...props }) => {
     return (
         <Wrapper className={className} style={style}>
-            <StyledSelect defaultValue={value} hasIcon="arrow_drop_down" isError={isError} {...props}>
+            <StyledSelect defaultValue={defaultValue} hasIcon="arrow_drop_down" isError={isError} {...props}>
                 {options.map(item => (
                     <option key={item.value} value={item.value}>{item.label}</option>
                 ))} 
