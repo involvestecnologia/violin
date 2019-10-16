d360d4c8749734914adb980cccf165bde6576997
Felipe Campos <felipecamposdev@gmail.com>
2019-10-16 18:55:35 -0300

    fix select value

--- src/lib/components/Select/Select.jsx
+++ src/lib/components/Select/Select.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyledSelect, Wrapper, StyledIcon } from "./style";
 
-export const Select = ({ options, defaultValue, className, style, isError, ...props }) => {
+export const Select = ({ options, className, style, isError, ...props }) => {
     return (
         <Wrapper className={className} style={style}>
-            <StyledSelect defaultValue={defaultValue} key={defaultValue} hasIcon="arrow_drop_down" isError={isError} {...props}>
+            <StyledSelect hasIcon="arrow_drop_down" isError={isError} {...props}>
                 {options.map(item => (
                     <option key={item.value} value={item.value}>{item.label}</option>
                 ))} 
@@ -17,11 +17,9 @@ export const Select = ({ options, defaultValue, className, style, isError, ...pr
 
 Select.propTypes = {
     options: PropTypes.array.isRequired,
-    defaultValue: PropTypes.any,
     hasSize: PropTypes.oneOf(["small", "normal", "large"])
 };
 
 Select.defaultProps = {
-    defaultValue: "",
     hasSize: "normal"
 };
