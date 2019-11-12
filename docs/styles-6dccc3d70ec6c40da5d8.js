343d06c3d58534c1bad28a6929ddcbb1e121305f
Felipe Campos <felipecamposdev@gmail.com>
2019-11-12 12:12:33 -0300

    config tests ok

---
+++ docs/styles-6dccc3d70ec6c40da5d8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{388:function(n,w,o){}}]);
+//# sourceMappingURL=styles-6dccc3d70ec6c40da5d8.js.map
---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  roots: ['<rootDir>/src/lib'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
