292c4d087ad3488850db9ad08389d9834ca9bb52
Felipe Campos <felipecamposdev@gmail.com>
2019-11-18 12:49:22 -0300

    config tests ok

--- docs/styles-5b95026abfb7b4eb3bf8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{182:function(n,w,o){}}]);
-//# sourceMappingURL=styles-5b95026abfb7b4eb3bf8.js.map
---
+++ src/lib/components/FormGroup/FormGroup.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import { FormGroup } from './FormGroup';
+
+afterEach(cleanup);
+
+describe('FormGroup', () => {
+	test('renders children', () => {
+		const { getByText } = render(<FormGroup>Lorem ipsum</FormGroup>);
+
+		expect(getByText('Lorem ipsum')).toBeInTheDocument();
+	})
+});
