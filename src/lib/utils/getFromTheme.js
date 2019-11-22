fd4155f0eaddcad019a0e7c75245b4b5e624e0c4
Felipe Campos <felipecamposdev@gmail.com>
2019-11-22 17:12:40 -0300

    change variable name from getFromTheme

--- src/lib/utils/getFromTheme.js
+++ src/lib/utils/getFromTheme.js
@@ -2,6 +2,6 @@ import { theme } from 'styled-tools';
 import { theme as involves } from '../theme';
 
 export default (path) => {
-  const valuePath = path.split('.').reduce((_, x) => _[x], involves);
-  return theme(path, valuePath);
+  const defaultThemePath = path.split('.').reduce((_, x) => _[x], involves);
+  return theme(path, defaultThemePath);
 }
