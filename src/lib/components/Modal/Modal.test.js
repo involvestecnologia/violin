de663f4056d65cfbf8d9679eb6fe02368405e6b7
onluiz <luiz.alberto1723@gmail.com>
2020-01-31 09:37:47 -0300

    Fix lint

--- src/lib/components/Modal/Modal.test.js
+++ src/lib/components/Modal/Modal.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import { Modal } from './Modal';
 
 afterEach(cleanup);
 
 describe('Modal', () => {
   test('should render correctly', () => {
+    const { getByTestId } = render(<div data-testid="modal">oi</div>);
+    expect(getByTestId('modal')).toBeInTheDocument();
   });
 });
