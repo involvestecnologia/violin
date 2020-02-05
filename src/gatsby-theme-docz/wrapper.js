89a41095f7abbf11851c5e04d72a252bbd78a130
Felipe Campos <felipecamposdev@gmail.com>
2020-02-05 18:37:25 -0300

    build

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,10 +1,17 @@
 import React from 'react';
 // eslint-disable-next-line import/no-unresolved
-import { GlobalStyle } from 'lib';
+import { GlobalStyle, theme, createTheme } from 'lib';
+import { ThemeProvider } from 'styled-components';
+
+const myCustomTheme = createTheme({
+  palette: {
+    primary: theme.palette.green
+  }
+});
 
 export default ({ children }) => (
-  <>
+  <ThemeProvider theme={myCustomTheme}>
     <GlobalStyle />
     {children}
-  </>
+  </ThemeProvider>
 )
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/lib/index.js'),
+  output: {
+    path: path.resolve(__dirname, './lib'),
+    filename: 'index.js',
+    library: '',
+    libraryTarget: 'commonjs'
+  },
+  externals: {
+    'styled-components': {
+      commonjs: 'styled-components',
+      commonjs2: 'styled-components',
+      amd: 'styled-components',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
