cbfd843284d415688e6dbc17716ee0a598bd64bb
Felipe Campos <felipecamposdev@gmail.com>
2020-02-06 14:55:55 -0300

    wip

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,17 +1,10 @@
 import React from 'react';
 // eslint-disable-next-line import/no-unresolved
-import { GlobalStyle, theme, createTheme } from 'lib';
-import { ThemeProvider } from 'styled-components';
-
-const myCustomTheme = createTheme({
-  palette: {
-    primary: theme.palette.green
-  }
-});
+import { GlobalStyle } from 'lib';
 
 export default ({ children }) => (
-  <ThemeProvider theme={myCustomTheme}>
+  <>
     <GlobalStyle />
     {children}
-  </ThemeProvider>
+  </>
 )
