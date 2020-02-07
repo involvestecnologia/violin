a204a0fc3dac7a106731f9de9d26486b76773902
Felipe Campos <felipecamposdev@gmail.com>
2020-02-07 13:34:54 -0300

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,11 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            'babel-plugin-styled-components'
+          ]
         }
       },
       {
