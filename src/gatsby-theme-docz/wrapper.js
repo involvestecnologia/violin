d5bec1d389d3e8ffba1a9dd886494ab6e56c5617
Felipe Campos <felipecamposdev@gmail.com>
2020-02-07 12:50:10 -0300

    build

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // eslint-disable-next-line import/no-unresolved
 import { GlobalStyle } from 'lib';
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
+import { Helmet } from 'react-helmet';
 
 const GlobalGatsbyStyle = createGlobalStyle`
   code {
@@ -11,10 +12,18 @@ const GlobalGatsbyStyle = createGlobalStyle`
   pre {
     border: 1px solid var(--theme-ui-colors-border,#CED4DE);
   }
+
+  [data-testid="live-preview"] {
+    padding: 20px;
+  }
 `
 
 export default ({ children }) => (
   <>
+    <Helmet>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" />
+    </Helmet>
     <GlobalGatsbyStyle />
     <GlobalStyle />
     {children}
