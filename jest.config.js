c23b5734831240ae4d7a9eca953a85f944a937af
onluiz <luiz.alberto1723@gmail.com>
2020-02-10 09:49:13 -0300

    Add enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src/lib'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    '<rootDir>src/setupTests.js'
+  ],
 };
