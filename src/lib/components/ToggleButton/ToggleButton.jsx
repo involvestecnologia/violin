b4b26c5e78167496fce6561333aec8a8fe9bfd18
Felipe Campos <felipecamposdev@gmail.com>
2020-02-11 20:10:03 -0300

    wip

---
+++ src/lib/components/ToggleButton/ToggleButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Wrapper, StyledButton, HiddenInput } from './style';
+
+export const ToggleButton = ({ multiple, style, className, children, ...props }) => (
+  <Wrapper>
+    <HiddenInput type={multiple ? 'checkbox' : 'radio'} {...props} />
+    <StyledButton style={style} className={className}>{children}</StyledButton>
+  </Wrapper>
+)
+
+ToggleButton.propTypes = {
+  /** Change to checkbox behavior */
+  multiple: PropTypes.bool,
+  /** Change style button */
+  style: PropTypes.oneOfType([PropTypes.object]),
+  /** Add css class to change button style */
+  className: PropTypes.string
+}
+
+ToggleButton.defaultProps = {
+  multiple: false,
+  style: null,
+  className: null
+}
