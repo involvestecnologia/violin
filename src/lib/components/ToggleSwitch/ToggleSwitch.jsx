2c83122981d55b001652205fc857c30b46631f2a
Felipe Campos <felipecamposdev@gmail.com>
2020-02-17 18:42:38 -0300

    test

--- src/lib/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/lib/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Wrapper, InputHidden, Svg, Rect, Circle, LabelText, Flex } from './style';
+import { Wrapper, InputHidden, Svg, Rect, Circle, LabelText } from './style';
 
 export const ToggleSwitch = ({ label, style, className, ...props }) => (
   <Wrapper data-testid="wrapper" style={style} className={className}>
     <InputHidden data-testid="input-hidden" type="checkbox" {...props} />
-    <Svg viewBox="0 0 44 24" mlns="http://www.w3.org/2000/svg">
+    <Svg data-testid="svg" viewBox="0 0 44 24" mlns="http://www.w3.org/2000/svg">
       <defs>
         <filter id="switch-shadow" x="-50%" y="-50%" width="200%" height="200%">
           <feOffset result="offOut" in="SourceGraphic" dx="0" dy="1" />
@@ -17,15 +17,27 @@ export const ToggleSwitch = ({ label, style, className, ...props }) => (
       <Rect x="4" y="6" rx="6" ry="6" />
       <Circle r="8" filter="url(#switch-shadow)" />
     </Svg>
-    {label && <LabelText>{label}</LabelText>}
+    {label && <LabelText data-testid="label">{label}</LabelText>}
   </Wrapper>
 )
 
 ToggleSwitch.propTypes = {
   /** Add text label to switch */
-  label: PropTypes.string
+  label: PropTypes.string,
+  /** Init checked (onChange is required) */
+  checked: PropTypes.bool,
+  /** Function to change state */
+  onChange: PropTypes.func,
+  /** Set style to container of component */
+  style: PropTypes.oneOfType([PropTypes.object]),
+  /** Set className to container of component */
+  className: PropTypes.string
 }
 
 ToggleSwitch.defaultProps = {
-  label: null
+  label: null,
+  checked: null,
+  onChange: null,
+  style: null,
+  className: null
 }
--- src/lib/components/ToggleSwitch/ToggleSwitch.test.js
+++ src/lib/components/ToggleSwitch/ToggleSwitch.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import { ToggleSwitch } from './ToggleSwitch';
 
 afterEach(cleanup);
@@ -8,5 +8,21 @@ describe('ToggleSwitch', () => {
   test('should render correctly', () => {
     const { getByTestId } = render(<ToggleSwitch />);
     expect(getByTestId('wrapper')).toBeInTheDocument();
+    expect(getByTestId('input-hidden')).not.toBeVisible();
+    expect(getByTestId('svg')).toBeInTheDocument();
+  });
+
+  test('should check when click', () => {
+    const { getByTestId } = render(<ToggleSwitch />);
+    expect(getByTestId('input-hidden')).not.toBeChecked();
+    fireEvent.click(getByTestId('wrapper'));
+    expect(getByTestId('input-hidden')).toBeChecked();
+  });
+
+  test('should display label text', () => {
+    const { queryByTestId, rerender } = render(<ToggleSwitch />);
+    expect(queryByTestId('label')).not.toBeInTheDocument();
+    rerender(<ToggleSwitch label="Check me!" />);
+    expect(queryByTestId('label')).toBeInTheDocument();
   });
 });
